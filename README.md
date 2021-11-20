f6424b2b6690bf00c534e499d5a7081f20f51d32
Mcrich23 <mcrich23@icloud.com>
2021-11-19 21:32:43 -0800

    Bug Fixes

--- README.md
+++
@@ -1,2 +0,0 @@
-# Screen-Saver
- A shortcut to show your selected screensaver from the MacOS Menu Bar
---
+++ iosScreenSaverUITests/iosScreenSaverUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iosScreenSaverUITestsLaunchTests.swift
+//  iosScreenSaverUITests
+//
+//  Created by Morris Richman on 11/19/21.
+//
+
+import XCTest
+
+class iosScreenSaverUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
